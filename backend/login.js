00a54b91fb43e26e994788aa18aeba80cc99ea9a
Gaurav Malakar <varuag2002@gmail.com>
2023-03-04 14:43:25 +0530

    Update login.js

--- backend/login.js
+++ backend/login.js
@@ -2,19 +2,28 @@
 const express = require('express');
 const loginRouter = express.Router();
 
+// Post request done while sign up
 loginRouter
 .route('/')
-.get(getLogin)
-.post(createUser);
+.post(createUser)
+.get(getLogin);
+
+// Get request done while login
+loginRouter
+.route('/?type=typ&employeeid=empid&password=pass')
 
 
-function getLogin(req, res){
-    res.send('Get Login');
-}
+
 
 function createUser(req, res){
     console.log(req.body);
     res.send('Login confirmed');
 }
+
+function getLogin(req, res){
+    res.send('Get Login request received');
+    console.log(req.query);
+}
+
 
 module.exports = loginRouter;
