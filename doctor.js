f341bf3fea6adba0856e7b6b7f427ea6cb54e1ef
Gaurav Malakar <varuag2002@gmail.com>
2023-03-04 21:51:40 +0530

    jj

--- doctor.js
+++ doctor.js
@@ -16,16 +16,44 @@ doctorRouter
 
 
 
+
+
 function getDoctor(req, res){
     res.send('<h1> Welcome To covid Hospital Management </h1>');
 }
 
+const queryTypes = {
+    1: 'treatment',
+    2: 'medication',
+    3: 'appointment'
+}
+
 function getDoctorByID(req, res){
     let id = req.params.id;
-    client.query('SELECT * FROM Physician WHERE EmployeeID = $1', [id], (err, result)=>{
-        if(err) throw err;
-        res.json(result.rows);
-    }); 
+    let query = req.query;
+
+    if(query == null){  // If no query is passed
+        client.query('SELECT * FROM Physician WHERE EmployeeID = $1', [id], (err, result)=>{
+            if(err) throw err;
+            res.json(result.rows);
+        });
+    }
+    else{
+        const type = queryTypes[query.type];
+        
+        if(type == 'treatment'){
+
+        }
+        else if(type == 'medication'){
+
+        }
+        else if(type == 'appointment'){
+
+        }
+
+    }
+
+
 }
 
 
