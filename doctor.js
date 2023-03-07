e46c7a9328eaf0a5dccf99c21b1ef4ee4a0e6af6
Gaurav Malakar <varuag2002@gmail.com>
2023-03-07 16:38:49 +0530

    admit done

--- doctor.js
+++ doctor.js
@@ -1,46 +1,48 @@
 
 // Route for doctor
 const express = require('express');
+
 const doctorRouter = express.Router();
+
 
 doctorRouter
 .route('/:id')
 .get(getDoctorByID)
- 
+
 
 
 function getDoctorByID(req, res){
     let id = req.params.id;
     let query = req.query;
-
+    
     req.db.getConnection((err, connection) => {
         if (err) {
             console.error('Error connecting to MySQL database: ', err);
             res.status(500).send('Internal Server Error');
             return;
         }
-
+        
         let sql_query = '';
-
+        
         if(Object.keys(query).length == 0)
-            sql_query = `SELECT * FROM Patient WHERE PCP=${id}`;
+        sql_query = `SELECT * FROM Patient WHERE PCP=${id}`;
         else{
             
             const type = query.type;
             const patient_id = query.patient;
-
+            
             if(patient_id == undefined){
                 res.status(400).send('Invalid query');
                 connection.release();
                 return;
             }
-
+            
             if(type == 'treatment')
-                sql_query = 'SELECT Patient.Name as "Patient Name",`Procedure`.Name as "Treatment Name",Date,Physician.Name as "Physician Name" FROM Undergoes,Physician,Patient,`Procedure` WHERE Patient='+patient_id+' and `Procedure`.Code=Undergoes.`Procedure` and Physician.EmployeeID=Undergoes.Physician and Patient.SSN=Undergoes.Patient';
+            sql_query = 'SELECT Patient.Name as "Patient Name",`Procedure`.Name as "Treatment Name",Date,Physician.Name as "Physician Name" FROM Undergoes,Physician,Patient,`Procedure` WHERE Patient='+patient_id+' and `Procedure`.Code=Undergoes.`Procedure` and Physician.EmployeeID=Undergoes.Physician and Patient.SSN=Undergoes.Patient';
             else if(type == 'medication')
-                sql_query = `SELECT Physician.Name as 'Physician Name',Patient.Name as 'Patient Name',Medication.Name as 'Medication Name',Medication.Brand as Brand,Date,Appointment as 'Appointment ID' FROM Prescribes,Medication,Physician,Patient WHERE Patient=${patient_id} and Medication.Code=Prescribes.Medication and Physician.EmployeeID=Prescribes.Physician and Patient.SSN=Prescribes.Patient`;
+            sql_query = `SELECT Physician.Name as 'Physician Name',Patient.Name as 'Patient Name',Medication.Name as 'Medication Name',Medication.Brand as Brand,Date,Appointment as 'Appointment ID' FROM Prescribes,Medication,Physician,Patient WHERE Patient=${patient_id} and Medication.Code=Prescribes.Medication and Physician.EmployeeID=Prescribes.Physician and Patient.SSN=Prescribes.Patient`;
             else if(type == 'appointment')
-                sql_query = `SELECT AppointmentID as 'Appointment ID',Patient.Name as 'Patient Name',Physician.Name as 'Physician Name',Start as 'Start Time',End as 'End Time' FROM Appointment,Patient,Physician WHERE Patient=${patient_id} and Patient.SSN=Appointment.Patient and Physician.EmployeeID=Appointment.Physician`;
+            sql_query = `SELECT AppointmentID as 'Appointment ID',Patient.Name as 'Patient Name',Physician.Name as 'Physician Name',Start as 'Start Time',End as 'End Time' FROM Appointment,Patient,Physician WHERE Patient=${patient_id} and Patient.SSN=Appointment.Patient and Physician.EmployeeID=Appointment.Physician`;
             else{
                 res.status(400).send('Invalid query type');
                 connection.release();
@@ -50,7 +52,7 @@ function getDoctorByID(req, res){
         }
         executeQuery(sql_query, res, connection);
         connection.release();
-    
+        
     });
 }
 
@@ -59,7 +61,7 @@ function formatDate(dateTimeStr) {
     const formattedDate = date.toISOString().slice(0, 10);
     const formattedTime = date.toTimeString().slice(0, 8);
     return `${formattedDate} ${formattedTime}`;
-  }
+}
 function executeQuery(sql_query, res, connection){
     connection.query(sql_query, (err, rows, fields) => {
         if(err){
@@ -88,5 +90,11 @@ function executeQuery(sql_query, res, connection){
         }
     });
 }
+
+
+
+// const prescribesRouter = require('./prescribes');
+// doctorRouter.use('/:id/prescribes', prescribesRouter);
+
 
 module.exports = doctorRouter;
