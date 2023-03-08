465f08f6d9cfc71919dd6a3178ce9fd04b88f448
Gaurav Malakar <varuag2002@gmail.com>
2023-03-08 18:40:48 +0530

    --

--- appointment.js
+++ appointment.js
@@ -11,52 +11,73 @@ appointmentRouter
 
 function getDoctorsToSchedule(req, res){
     
+
+    let data = {doctors: [], patients: []};
+    let get_doctors = 'SELECT Name, PhysicianID AS id, Position FROM Physician INNER JOIN User ON Physician.PhysicianID = User.EmployeeID;';
+    let get_patients = 'SELECT Patient_Name, Patient_SSN AS id, Age, Gender FROM Patient;';
+
     req.db.getConnection((err, connection) => {
         if (err) {
             console.error('Error connecting to MySQL database: ', err);
             res.status(500).send('Internal Server Error');
             return;
         }
-        let sql_query = 'SELECT * FROM Physician;'
-        executeQueryResp(sql_query, res, connection);
+        connection.query(get_doctors, (err, rows) => {
+            if (err) {
+                console.error('Error executing MySQL query: ', err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+            data.doctors = rows;
+        });
 
- 
-
- 
-        connection.release();
+        connection.query(get_patients, (err, rows) => {
+            if (err) {
+                console.error('Error executing MySQL query: ', err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+            data.patients = rows;
+            res.status(200).send(data);
+        });
     });
 
 }
 
 
 function getSlots(req, res){
+    const req_date=req.query.date;
+    const doc_id=req.query.docId;
+    let sql_query = `SELECT  Start,End FROM Appointment, Slot WHERE PhysicianID=${doc_id} AND Date='${req_date}' AND SlotID NOT IN ((SELECT SlotID FROM Appointment WHERE Date='${req_date}' and PhysicianID='${doc_id}') INTERSECT (SELECT Start,End FROM Slot WHERE PhysicianID=${doc_id} AND Date='${req_date}' AND SlotID NOT IN (SELECT SlotID FROM Treatment WHERE Date='${req_date}' and PhysicianID='${doc_id}')));`;
+    executeQuery(sql_query, req, res);
+}
+
+
+
+function executeQuery(sql_query, req, res){  
+    let status = 200, message = 'OK', data = null;
     req.db.getConnection((err, connection) => {
         if (err) {
             console.error('Error connecting to MySQL database: ', err);
-            res.status(500).send('Internal Server Error');
+            status = 500;
+            message = 'Internal Server Error';
+            res.status(status).send({message});
             return;
         }
-        let doctor_id = req.query.doctor_id;
-        let date = req.query.date;
+        
+        connection.query(sql_query, (err, rows, fields) => {
+            if(err){
+                status= 500;
+                message = 'Internal Server Error';
+                console.log(err);
+            }
+           data = {...rows};
+           res.status(status).send({message, data});
+           console.log({status, message});
+        });
 
-
-
-        let sql_query = 'SELECT * FROM Physician;'  // write query here
-
-
-        executeQueryResp(sql_query, res, connection);
         connection.release();
-    });
-}
-
-function executeQueryResp(sql_query, res, connection){  
-    connection.query(sql_query, (err, rows, fields) => {
-        if(err){
-            res.status(500).send('Internal Server Error');
-            console.log(err);
-        }
-        else
-            res.status(200).send({data: rows});
+        //return {status: status, message: message, data: data};
     });
 }
 
