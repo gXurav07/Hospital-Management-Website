1c2da1cef4b52ba63c6a753786c79bc108c7667b
Gaurav Malakar <varuag2002@gmail.com>
2023-03-12 02:11:56 +0530

    progress

--- admin.js
+++ admin.js
@@ -7,61 +7,65 @@ adminRouter
 .post(createEntity)
 //.delete(deleteEntity);
 
-function createEntity(req, res){
-    console.log('Hi\n', req.body);
+async function createEntity(req, res){
+    
     const entity = req.body;
     const type = req.body.type;
     let sql_query = '';
+    let result = null;
 
-    req.db.getConnection((err, connection) => {
-        if (err) {
-            console.error('Error connecting to MySQL database: ', err);
-            res.status(500).send('Internal Server Error');
+
+    if(type == 'user'){
+        sql_query =  `INSERT INTO User(UserID, Name, Password) `+
+                        `VALUES('${entity.employeeid}', '${entity.name}', '${entity.password}'); `;
+    }
+    else if(type == 'doctor'){
+        sql_query =  `INSERT INTO Physician(PhysicianID, Name, Position, SSN) `+
+                        `VALUES('${entity.employeeid}', '${entity.name}', '${entity.position}', 23213); `;
+        result = await executeQuery(sql_query, res, connection);
+
+        if(result.status != 200){
+            res.status(result.status).send(result);
             return;
         }
 
+        sql_query  = `INSERT INTO Affiliated_with(Physician, Department, PrimaryAffiliation) `+
+                     `VALUES('${entity.employeeid}', ${entity.department}, False);`;
 
-        if(type == 'doctor'){
-            sql_query =  `INSERT INTO Physician(EmployeeID, Name, Position, SSN) `+
-                         `VALUES('${entity.employeeid}', '${entity.name}', '${entity.position}', 23213); `;
-            executeQuery(sql_query, res, connection);
+                        
+    }
+    else if(type == 'data-entry'){
+        sql_query =  `INSERT INTO DataEntry(EmployeeID, Name, Address) `+
+                     `VALUES('${entity.employeeid}', '${entity.name}', '${entity.address}'); `;
+    }
+    else if(type=='medication'){
+        sql_query = `INSERT INTO Medication(MedicationID, Medicationn_Name, Brand, Description) `+
+                    `VALUES('${entity.medicationid}', '${entity.name}', '${entity.brand}', '${entity.description}'); `;
 
-            sql_query  = `INSERT INTO Affiliated_with(Physician, Department, PrimaryAffiliation) `+
-                         `VALUES('${entity.employeeid}', ${entity.department}, False);`;
-            executeQuery(sql_query, res, connection);
-                            
-        }
+    }
+    else if(type=='department'){
+        sql_query = `INSERT INTO Department(DepartmentID, Dep_Name, Head) `+
+                    `VALUES(${entity.departmentid}, '${entity.name}', '${entity.head}'); `;
+    }
+    else if(type=='test'){
+        sql_query = `INSERT INTO Test(TestID, Test_Name, Cost) `+
+                    `VALUES('${entity.testid}', '${entity.name}', '${entity.cost}'); `;
+    }
+    else if(type=='treatment'){
+        sql_query = `INSERT INTO Treatment_Description(Treatment_DescriptionID, Desc_Name, Cost) `+
+                    `VALUES('${entity.treatmentid}', '${entity.name}', '${entity.cost}'); `;
+    }
+    else{
+        res.status(400).send({message: 'Invalid type'});
+        return;
+    }
 
-        else{
-            res.status(400).send('Invalid query type');
-            connection.release();
-            return;
-        }
-        
-    res.status(200).send({message: 'Successfuly created entity!'});
-    connection.release();
+    result = await executeQuery(sql_query, res, connection);
+    res.status(result.status).send(result);
 
-    //res.status(200).send({message: `Successfuly created ${type}!`});
-
-        
-    
-    });
+  
 }
 
-function createUser(entity, res, connection){
-    const sql_query = `INSERT INTO user(employee_id, Name, Email, Password, iv, type)\
-                       VALUES(${entity.id}, '${entity.name}', '${entity.email}', '${entity.password}', '${entity.pass_iv}', ${entity.type});`
-    executeQuery(sql_query, res, connection);
-}
 
-function executeQuery(sql_query, res, connection){
-
-    connection.query(sql_query, (err, rows, fields) => {
-        if(err){
-            res.status(500).send('Internal Server Error');
-            console.log(err);
-        }
-    });
-}
 
 module.exports = adminRouter;
