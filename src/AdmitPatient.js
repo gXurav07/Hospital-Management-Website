e00c486cbff8da85d6cffc33219e26677313a94d
priydarshiroopak <priydarshiroopak@gmail.com>
2023-03-12 19:00:35 +0530

    comment changes

--- src/AdmitPatient.js
+++ src/AdmitPatient.js
@@ -24,23 +24,23 @@ function AdmitPatient(props) {
             Header: 'Patient SSN',
             accessor: 'id',
             Cell: ({ cell: { value } }) => value || "-",
-                    },
+        },
         {
             Header: 'Name',
             accessor: 'Patient_Name',
             Cell: ({ cell: { value } }) => value || "-",
-                    },
+        },
         {
             Header: 'Age',
             accessor: 'Age',
             Cell: ({ cell: { value } }) => value || "-",
-                        Filter: SelectColumnFilter,
+            Filter: SelectColumnFilter,
         },
         {
             Header: 'Gender',
             accessor: 'Gender',
             Cell: ({ cell: { value } }) => value || "-",
-                        Filter: SelectColumnFilter,
+            Filter: SelectColumnFilter,
         }
     ];
 
@@ -68,19 +68,24 @@ function AdmitPatient(props) {
         e.preventDefault();
         const admit_patient = {patient: patientId, room: roomId};
         console.log(admit_patient);
-        fetch('http://' + server_addr + '/front-desk/admit', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(admit_patient)
-        })
-        .then(res => {
-            return res.json();
-          })
-          .then(data => {
+        if(patientId !== '' && roomId !== '') {
             console.log(admit_patient);
-            setResult(data);
+            setResult(admit_patient);
             alert(result);
-          });
+            // fetch('http://' + server_addr + '/front-desk/admit', {
+            //     method: 'POST',
+            //     headers: { "Content-Type": "application/json" },
+            //     body: JSON.stringify(admit_patient)
+            // })
+            // .then(res => {
+            //     return res.json();
+            // })
+            // .then(data => {
+            // });
+        }
+        else {
+            setResult("Please fill all the fields");
+        }
     }
 
     return (
@@ -90,17 +95,15 @@ function AdmitPatient(props) {
                     <h1>Admit a Patient</h1>
                     {/* {patients ? <Table data={patients} /> : <br />} */}
                     <div className='form_wrapper'>
-                        <form>
-                            {/* <label>ID:</label> */}
-                            {/* <input type="text" placeholder="Enter Patient ID...." required value={pid} onChange={(e) => setPid(e.target.value)} /> */}
-                            <Col sm={{offset: 3, size: 6}}> Select{(patientId!=='')?"ed":""} Patient ID: {patientId}</Col>
-                            {(patients.length >0) ? <TableContainer columns={patientColumns} data={patients} selectedRow={patientId} setSelectedRow={(row) => setPatientId(row.values['id'])} TableName="Patients" identifierColumn={'id'}/> : <><p>Sorry! Unable to fetch Patient data from server.</p><br/></>}
-                            <br/>
-                            <Col sm={{offset: 3, size: 6}}> Select{(roomId!=='')?"ed":""} Room ID: {roomId}</Col>
-                            {(rooms.length > 0) ? <TableContainer columns={roomColumns} data={rooms} selectedRow={roomId} setSelectedRow={(row) => setRoomId(row.values['id'])} TableName="Rooms" identifierColumn={'id'}/> : <><p>Sorry! Unable to fetch Room data from server.</p><br/></>}
-                            <br/>
-                            <button className='box' onSubmit={(e)=>handleAdmit(e)}>Admit</button>
-                        </form>
+                        {/* <label>ID:</label> */}
+                        {/* <input type="text" placeholder="Enter Patient ID...." required value={pid} onChange={(e) => setPid(e.target.value)} /> */}
+                        <Col sm={{offset: 3, size: 6}}> Select{(patientId!=='')?"ed":""} Patient ID: {patientId}</Col>
+                        {(patients.length >0) ? <TableContainer columns={patientColumns} data={patients} selectedRow={patientId} setSelectedRow={(row) => setPatientId(row.values['id'])} TableName="Patients" identifierColumn={'id'}/> : <><p>Sorry! Unable to fetch Patient data from server.</p><br/></>}
+                        <br/>
+                        <Col sm={{offset: 3, size: 6}}> Select{(roomId!=='')?"ed":""} Room ID: {roomId}</Col>
+                        {(rooms.length > 0) ? <TableContainer columns={roomColumns} data={rooms} selectedRow={roomId} setSelectedRow={(row) => setRoomId(row.values['id'])} TableName="Rooms" identifierColumn={'id'}/> : <><p>Sorry! Unable to fetch Room data from server.</p><br/></>}
+                        <br/>
+                        <button type='submit' className='box' onClick={(e)=>handleAdmit(e)}>Admit</button>
                     </div>
                 </div>
             </header>
