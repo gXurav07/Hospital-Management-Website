1397db9219c99039f3164ff784e99d88856b197d
Gaurav Malakar <varuag2002@gmail.com>
2023-03-12 23:33:55 +0530

    do mail

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ app.listen(port_no, function(){
 // Use the connection pool in your Express app
 app.use((req, res, next) => {
     console.log('\n\nmethod: ', req.method);
-    console.log('body: ', req.body);
-    console.log('query: ', req.query);
-    console.log('params: ', req.params, '\n\n');
+    console.log('url: ', req.url);
+    console.log('body: ', req.body, '\n\n');
 
     req.db = pool;
     next();
