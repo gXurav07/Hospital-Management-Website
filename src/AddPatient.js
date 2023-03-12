7f7d3dfbd6602a5fbff0dc3c736e1d3037bab46d
priydarshiroopak <priydarshiroopak@gmail.com>
2023-03-12 19:31:47 +0530

    css

--- src/AddPatient.js
+++ src/AddPatient.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FormGroup, Label, Col, Input, Form, Button } from "reactstrap";
 
 function AddPatient(props) {
     const [ssn, setSSN] = useState('');
@@ -31,7 +32,57 @@ function AddPatient(props) {
             </header>
             <div className="App-body">
                 <div className="managedocs">
-                    <form onSubmit={handleSubmit}>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup row>
+                            <Label for="ssn" sm={3}>SSN</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="ssn" id="ssn" placeholder="Enter patient SSN....." required value={ssn} onChange={(e) => setSSN(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="name" sm={3}>Name</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="name" id="name" placeholder="Enter name of patient....." required value={name} onChange={(e) => setName(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="addr" sm={3}>Address</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="addr" id="addr" placeholder="Enter address of patient...." required value={addr} onChange={(e) => setAddr(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="age" sm={3}>Age</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="age" id="age" placeholder="Enter age of patient...." required value={age} onChange={(e) => setAge(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="phone" sm={3}>Phone</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="phone" id="phone" placeholder="Enter phone number of patient...." required value={phone} onChange={(e) => setPhone(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="insuranceID" sm={3}>Insurance ID</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="insuranceID" id="insuranceID" placeholder="Enter insurance ID of patient...." required value={insuranceID} onChange={(e) => setInsuranceID(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="pcp" sm={3}>PCP</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="pcp" id="pcp" placeholder="Enter PCP of patient...." required value={pcp} onChange={(e) => setPCP(e.target.value)} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup check row>
+                            <Col sm={{ size: 9, offset: 3 }}>
+                                <Button sm={3}>Add Patient</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+
+                    {/* <form onSubmit={handleSubmit}>
                         <label>SSN:</label>
                         <input type="text" placeholder="Enter patient SSN....." required value={ssn} onChange={(e) => setSSN(e.target.value)} />
                         <label>Name:</label>
@@ -53,7 +104,7 @@ function AddPatient(props) {
                         <label>PCP:</label>
                         <input type="text" placeholder="Enter PCP of patient...." required value={pcp} onChange={(e) => setPCP(e.target.value)} />
                         <button>Add Patient</button>
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </div>
